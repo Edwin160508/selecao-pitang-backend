274c52b8d8d00dcb0d8b6a82d95ca21af5da0f29
Edwin Lima <edwin.limaa@gmail.com>
2024-04-10 09:22:33 -0300

    Create project

---
+++ src/test/java/br/com/edwin/lima/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.edwin.lima;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
