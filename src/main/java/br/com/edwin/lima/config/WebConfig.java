f5550154c12fcc0bd250fed56b916d0ae41c947c
Edwin Lima <edwin.limaa@gmail.com>
2024-04-15 11:51:36 -0300

    Configure CORS origin.

---
+++ src/main/java/br/com/edwin/lima/config/WebConfig.java
@@ -0,0 +1,32 @@
+package br.com.edwin.lima.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("\\${cors.originPatterns:default}")
+    private String corsOriginPatterns="";
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON);
+    }
+
+    public void addCorsMappings(CorsRegistry registry){
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .maxAge(3600)
+            .allowCredentials(false);
+    }
+}
